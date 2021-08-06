e9089920ff70a84052d46966136956a362aeafbb
RajrupDasid <rajrupdasid@outlook.com>
2021-08-06 12:25:09 +0530

    all necessary settings added  and image bug fixed

--- customers/urls.py
+++ customers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import render_pdf_view
+from .views import render_pdf_view,CustomerListView,crpv
 app_name='customers'
 urlpatterns=[
-    path('',render_pdf_view,name='test-view'),
+    path('',CustomerListView.as_view(),name='customer-listview'),
+    path('test/',render_pdf_view,name='test-view'),
+    path('pdf/<pk>/',crpv,name='cprv'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
@@ -11,9 +11,28 @@ class CustomerListView(ListView):
    template_name='customers/main.html'
 
 #customer render pdf view
-def crpv(request,*args,**kowargs):
-   pass
+def crpv(request,*args,**kwargs):
+   pk=kwargs.get('pk')
+   customer=get_object_or_404(Customer,pk=pk)
+   template_path = 'customers/pdf2.html'
+   context = {'customer': customer}
+   # Create a Django response object, and specify content_type as pdf
+   response = HttpResponse(content_type='application/pdf')
+   #if download run this  code 
+   response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+   #if dispay set attachment
+   response['Content-Disposition'] = 'filename="report.pdf"'
+   # find the template and render it.
+   template = get_template(template_path)
+   html = template.render(context)
 
+   # create a pdf
+   pisa_status = pisa.CreatePDF(
+      html, dest=response)
+   # if error then show some funy view
+   if pisa_status.err:
+      return HttpResponse('We had some errors <pre>' + html + '</pre>')
+   return response
 
 
 
